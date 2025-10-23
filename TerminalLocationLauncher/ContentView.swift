3263b30a894fe2e8c954c8c3e83f23cb0894e48d
Corey Lofthus <corey@soulyftaudio.com>
2025-10-23 13:24:55 -0700

    Initial Commit

---
+++ TerminalLocationLauncher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TerminalLocationLauncher
+//
+//  Created by Corey Lofthus on 10/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TerminalLocationLauncher/TerminalLocationLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalLocationLauncherApp.swift
+//  TerminalLocationLauncher
+//
+//  Created by Corey Lofthus on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TerminalLocationLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
